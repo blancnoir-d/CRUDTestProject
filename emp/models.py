961f084e0f9b6ef3e66af4a51530a43a6fb891a8
Saeha <blancnoir.d@gmail.com>
2022-01-21 11:43:24 +0900

    model 생성

--- emp/models.py
+++ emp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class employee(models.Model):
+    emp_name = models.CharField(max_length=30)
+    team_number = models.IntegerField()
+    emp_rank = models.CharField(max_length=30)
+    emp_responsibilities = models.CharField(max_length=50)
+    monthly_salary = models.IntegerField()
+    entry_date = models.DateField(null=True)
+
+
+
--- hrd/models.py
+++ hrd/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 
+class team(models.Model):
+    team_name = models.CharField(max_length=30)
+    team_position = models.CharField(max_length=100)
+    team_explanation = models.TextField()
+
+

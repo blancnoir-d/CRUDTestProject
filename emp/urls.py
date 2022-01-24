3aade9961e36425272a916731dc8e8f194fbf307
Saeha <blancnoir.d@gmail.com>
2022-01-24 10:12:00 +0900

    사원 등록, 사원 수정 기능 추가

--- emp/urls.py
+++ emp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.EmpList.as_view()),
-    path('<int:pk>/', views.EmpDetail.as_view()),
+    path('<int:pk>/', views.EmpDetail.as_view()), #emp/숫자
+    path('create_emp/', views.EmpCreate.as_view()),
+    path('update_emp/<int:pk>/', views.EmpUpdate.as_view()),
 ]
--- emp/views.py
+++ emp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import employee
+
+# CreateView 하면서 추가 된 부분
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
@@ -11,3 +15,33 @@ class EmpList(ListView):
 
 class EmpDetail(DetailView):
     model = employee
+
+
+class EmpCreate(CreateView, LoginRequiredMixin, UserPassesTestMixin):
+    model = employee
+    fields = ['emp_name', 'team_number', 'emp_rank', 'emp_responsibilities', 'monthly_salary', 'entry_date', 'image']
+
+    def CheckUser(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
+            form.instance.author = current_user
+            response = super(EmpCreate, self).form_valid(form)
+            return response
+        else:
+            return redirect('/emp/')
+
+
+class EmpUpdate(UpdateView, LoginRequiredMixin):
+    model = employee
+    fields = ['emp_name', 'team_number', 'emp_rank', 'emp_responsibilities', 'monthly_salary', 'entry_date', 'image']
+
+    def get_context_data(self, **kwargs):
+        context = super(EmpUpdate, self).get_context_data()
+        return context
+
+    def form_valid(self, form):
+        response = super(EmpUpdate, self).form_valid(form)
+        return response
